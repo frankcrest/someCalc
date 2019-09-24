2e82f18944897e022bee71d30ba9b0d14b5477d8
Frank Chen <frankcrestview@hotmail.com>
2019-09-24 16:00:24 -0400

    Initial Commit

---
+++ someCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  someCalculator
+//
+//  Created by Frank Chen on 2019-09-24.
+//  Copyright © 2019 Frank Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
